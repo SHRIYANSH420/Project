8ea6499c4201ba96e4e77f6b3b5c84a4105446af
Shriyansh <rauthanshriyansh@gmail.com>
2025-02-01 20:46:16 +0530

    creating frontend in vite with tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-100">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
