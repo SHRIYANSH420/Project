ed42b654ca7d8cacba12820247278e11755c36d2
Shriyansh <rauthanshriyansh@gmail.com>
2025-02-01 21:07:46 +0530

    creating Home and Admin pages with api section needed backend first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import FAQ from "./pages/Faq";
+import Admin from "./pages/Admin";
 
 const App = () => {
   return (
-    <div>
-      <div className="container bg-red-100">
-        <h1>Hello World</h1>
+    <Router>
+      <div className="p-4">
+        <nav className="mb-4 flex space-x-4">
+          <Link to="/" className="text-blue-500">Home</Link>
+          <Link to="/admin" className="text-blue-500">Admin</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<FAQ />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
       </div>
-    </div>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
